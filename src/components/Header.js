fea9684110c3c6533d7a53b2dcbd5de5daa757b9
Irene de Koning <irene@zooma.nl>
2021-05-13 20:48:22 +0200

    INITIAL COMMIT

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '../../index.css';
+import headerImage from '../../../portfolioirene/src/assets/images/header.jpeg';
+
+const header = () => {
+    return(
+        <header className="w-full">
+            <img className="w-full h-96 object-cover" src={headerImage} alt="Header Image"/>
+        </header>
+    )
+};
+
+export default header;
